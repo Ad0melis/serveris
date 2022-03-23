41e9b06bed4d8b321fb660535aa816b5ff72383c
Ad0melis <96545771+Ad0melis@users.noreply.github.com>
2022-03-23 20:39:07 +0200

    Initial page content on rout

---
+++ pages/PageLogin.js
@@ -0,0 +1,10 @@
+import { PageTemplate } from "../lib/PageTemplate.js";
+
+
+class PageLogin extends PageTemplate {
+    mainHTML(){
+        return 'LOGIN PAGE CONTENT'
+    }
+}
+
+export {PageLogin}
---
+++ pages/PageRegister.js
@@ -0,0 +1,10 @@
+import { PageTemplate } from "../lib/PageTemplate.js";
+
+
+class PageRegister extends PageTemplate {
+    mainHTML(){
+        return 'REGISTER PAGE CONTENT'
+    }
+}
+
+export {PageRegister}
